8b9d4981bcbb77ffbfe24a040b9c3c438f79d925
Mike Møller Nielsen <mmni@protonmail.com>
2021-06-04 15:00:04 +0200

    files

---
+++ initgit.sh
@@ -0,0 +1,7 @@
+echo "# grails-demo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:ekim197711/grails-demo.git
+git push -u origin main
